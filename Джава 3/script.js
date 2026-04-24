cad7130b7b0873547c22c7c462cec8ef9a06d082
noname1444 <silviagavrilenko@gmail.com>
2026-04-24 17:41:30 +0300

    Create script.js

---
+++ Джава 3/script.js
@@ -0,0 +1,27 @@
+let OneProposal = "Летние каникулы я провела дома, в городе.";
+let TwoProposal = "Каждый день я играла на улице со своими друзьями.";
+let ThreeProposal = "Мне было весело." ;
+let FourProposal = "Мы катались на велосипедах, собирали из конструктора роботов.";
+let FiveProposal = "Еще мы ходили с друзьями на спортивную площадку.";
+let SixProposal = "Я хорошо провела время на летних каникулах.";
+let SevenProposal = "Сочные цвета деревьев, яркое голубое небо с небольшими белыми облаками, прекрасные полевые цветы радующие глаз человека.";
+let EightProposal = "Волшебный закат встречал меня каждый вечер!";
+let NineProposal = "До скорой встречи!";
+let TenProposal = "Лето - моё любимое время года.";
+let ElevenProposal = "Летние каникулы - это самый долгий отдых от учёбы.";
+let TwelveProposal = "В жаркие дни я ходила на речку. ";
+
+let OneSymbol = OneProposal[2]
+let TwoSymbol = TwoProposal[4]
+let ThreeSymbol = ThreeProposal[8]
+let FourSymbol = FourProposal[9]
+let FiveSymbol = FiveProposal[29]
+let SixSymbol = SixProposal[4]
+let SevenSymbol = SevenProposal[11]
+let EightSymbol = EightProposal[0]
+let NineSymbol = NineProposal[16]
+let TenSymbol = TenProposal[0]
+let ElevenSymbol = ElevenProposal[22]
+let TwelveSymbol = TwelveProposal[13]
+
+console.log(`${OneSymbol}${TwoSymbol}${ThreeSymbol}${FourSymbol}${FiveSymbol}${SixSymbol}${SevenSymbol}${EightSymbol}${NineSymbol}${TenSymbol}${ElevenSymbol}${TwelveSymbol}`.toLowerCase())
